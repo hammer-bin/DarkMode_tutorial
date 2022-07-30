885957990a202fb0b6acf5f1e1cfeac23de839c7
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-31 03:52:39 +0900

    Initial Commit

---
+++ DarkMode_tutorial/DarkMode_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkMode_tutorialApp.swift
+//  DarkMode_tutorial
+//
+//  Created by minkyuLee on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct DarkMode_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
